ee056945e6c631cd8fbcbe027e689bfd01269a08
iamumanggoel <104354651+iamumanggoel@users.noreply.github.com>
2026-04-15 19:27:19 +0530

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var seen = new Dictionary<int, int>(); // { val: idx }
+
+        for(int i = 0; i < nums.Length; i++){
+            int comp = target - nums[i];
+
+            if(seen.ContainsKey(comp))
+                return new int[2] { seen[comp], i };
+
+            seen[nums[i]] = i;
+        }
+
+        return new int[0];
+    }
+}
